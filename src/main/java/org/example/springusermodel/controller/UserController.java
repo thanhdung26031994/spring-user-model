4364681fe5519c2a1ab0467a1101b8b48048e15e
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-03-03 12:40:32 +0700

    login user

--- src/main/java/org/example/springusermodel/controller/UserController.java
+++ src/main/java/org/example/springusermodel/controller/UserController.java
@@ -1,2 +1,36 @@
-package org.example.springusermodel.controller;public class UserController {
+package org.example.springusermodel.controller;
+
+import org.example.springusermodel.DAO.UserDAO;
+import org.example.springusermodel.model.Login;
+import org.example.springusermodel.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+    @GetMapping("/home")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("login", new Login());
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login){
+        User user = UserDAO.checkLogin(login);
+        ModelAndView modelAndView;
+        if (user == null){
+            modelAndView = new ModelAndView("error");
+        }else {
+            modelAndView = new ModelAndView("user");
+            modelAndView.addObject("user", user);
+
+        }
+        return modelAndView;
+    }
 }
